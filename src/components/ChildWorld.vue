27f0390b72d11858262f4a3fdfaa1edf21271a46
bai <zuiqia1@163.com>
2019-03-04 14:43:48 +0800

    init store

---
+++ src/components/ChildWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="childWorld">
+    <h1>{{msg}}</h1>
+    <p>{{age}}</p>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Component, Vue, Prop } from 'vue-property-decorator'
+@Component({})
+export default class ChildWorld extends Vue {
+  msg = 'childWorld'
+  @Prop({type: Number, default: 0})
+  age
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <child-world :age = age></child-world>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import ChildWorld from './ChildWorld.vue'
+@Component({
+  components: {
+    ChildWorld
+  }
+})
+export default class HelloWorld extends Vue {
+  msg = 'Welcome to Your Vue.js App'
+  age = 12
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
